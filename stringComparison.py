f0f9566cde7944614f7457d0aecd94bd025446b7
Hendrix33 <jnhendrix33@gmail.com>
2019-12-19 10:49:59 -0500

    Add files via upload

---
+++ stringComparison.py
@@ -0,0 +1,9 @@
+from __future__ import print_function
+from __future__ import unicode_literals
+
+myString = 'fe80:cd00:0000:0cde:1257:0000:211e:729c'
+myCaps = "FE80:CD00:0000:0CDE:1257:0000:211E:729C"
+myNumbers = "fe80:CD00:0000:0CDe:1257:0000:211E:729C"
+
+myString == myCaps
+print(myString == myCaps)
---
+++ timeConversion.py
@@ -0,0 +1,7 @@
+import sys
+
+userTime = input('Please enter a time in minutes')
+minute = int(userTime)
+seconds = 60 * minute
+
+print(seconds)
